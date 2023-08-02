ae197cbbd95f0a0a04b2ed7181fe0b4626934547
brunovmc <bruno.vidigal.coriolano@gmail.com>
2023-08-02 15:43:47 -0300

    feat(index.js)add user route

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const users = [];
+
+app.use(express.json());
+
+app.post('/add-user', (req, res) => {
+    const newUser = req.body;
+
+    users.push(newUser);
+
+    res.json(users);
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.send(users);
 });
 
 app.listen(port, () => {
